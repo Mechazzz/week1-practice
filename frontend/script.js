d4ac3bba5aecb9f0da877bc5d4706bd8202ff19a
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2023-09-15 20:44:56 +0200

    basic layout and js practice

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("I am Istvan");
+//setInterval(()=>console.log("Hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x + y;
+let z = "Csésze"
+console.log(z);
+z = "Bögre"
+console.log(z);
